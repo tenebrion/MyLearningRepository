851359b8fdb3d8dc64569cd7a1e6ee0338009913
Michael Koegel <mmkoegel@gmail.com>
2012-05-22 21:31:21 -0700

    Here are a couple other examples I am working on. Nothing fancy, just teaching myself new things.

---
+++ ReturnDistinctValues/ReturnDistinctValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReturnDistinctValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userNumbers = { 1, 3, 5, 3, 7, 3, 1, 1, 5 };
+
+            HashSet<int> values = new HashSet<int>(userNumbers);
+
+            foreach (int i in values)
+            {
+                Console.WriteLine("{0}", i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReverseString/ReverseString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence");
+            // taking user input and assigning it to userInput
+            string userInput = Console.ReadLine();
+
+            /// <summary>
+            /// This section is flipping between string and char so
+            /// the program will reverse the order of the string a
+            /// user enters
+            /// </summary>
+            //char[] userInputArray = userInput.ToCharArray();
+            //Array.Reverse(userInputArray);
+            //string output = new string(userInputArray);
+            //Console.Write("Here is your line in reverse: {0}", output);
+
+            /// <summary>
+            /// This section with utilize LINQ to reverse a user string
+            /// </summary>
+            // the initial information is getting pulled from the
+            string output = new string(userInput.ToCharArray().Reverse().ToArray());
+            Console.WriteLine("Here is your line in reverse: {0}", output);
+
+            Console.ReadLine();
+        }
+    }
+}
